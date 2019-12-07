f1ed04c024336cd51cb877869edb28a32c8a6680
Yuri Kilochek <yuri.kilochek@gmail.com>
2019-12-08 01:54:21 +0300

    WIP.

--- include/half.hpp
+++ include/half.hpp
@@ -1,13 +1,5 @@
-#ifndef HALF_INCLUDED
-#define HALF_INCLUDED
-
-#if __cplusplus < 201703L
-    #error "Requires C++17 or later."
-#endif
-
-#if __has_include(<emmintrin.h>)
-    #include <emmintrin.h>
-#endif
+#ifndef HALFFLOAT_DETAIL_INCLUDED_HPP
+#define HALFFLOAT_DETAIL_INCLUDED_HPP
 
 #include <cstdint>
 #include <cstddef>
@@ -15,184 +7,87 @@
 #include <climits>
 #include <cstring>
 #include <type_traits>
-#if __cplusplus > 201703L
-    #include <bit>
-#endif
 
-namespace half_detail {
-    #if __cplusplus > 201703L
-        using std::bit_cast;
-    #else
-        template <typename To, typename From>
-        auto bit_cast(From from)
-        -> To
-        {
-            static_assert(std::is_trivial_v<From>);
-            static_assert(std::is_trivial_v<To>);
-            static_assert(sizeof(To) == sizeof(From));
+namespace halffloat {
+///////////////////////////////////////////////////////////////////////////////
 
-            To to;
-            std::memcpy(&to, &from, sizeof(to));
-            return to;
-        }
-    #endif
-
-
-    //template <typename T>
-    //struct float_traits;
-
-    //template <>
-    //struct float_traits<float> {
-    //    static constexpr
-
-    //};
-
-    template <typename T>
-    auto bits_from_floating_point(T value)
-    -> std::uint16_t
+namespace detail {
+    template <typename To, typename From>
+    auto bit_cast(From from)
+    -> To
     {
-        return {};
+        static_assert(sizeof(To) == sizeof(From));
+        static_assert(std::is_trivially_copyable_v<From>);
+        static_assert(std::is_trivial_v<To>);
+
+        To to;
+        std::memcpy(&to, &from, sizeof(to));
+        return to;
     }
-
-    template <typename T>
-    auto bits_from_integral(T value)
-    -> std::uint16_t
-    {
-        return {};
-    }
-
-    template <typename T>
-    auto bits_from_unsigned_integral(T value)
-    -> std::uint16_t
-    {
-        return {};
-    }
-
-    template <typename T>
-    auto bits_to_floating_point(std::uint16_t bits)
-    -> T
-    {
-        return {};
-    }
-
-    template <typename T>
-    auto bits_to_integral(std::uint16_t bits)
-    -> T
-    {
-        return {};
-    }
-
-    template <typename T>
-    auto bits_to_unsigned_integral(std::uint16_t bits)
-    -> T
-    {
-        return {};
-    }
-
-    //single_to_half() {
-    //    #ifdef __F16C__
-    //        if (!std::is_constant_evaluated()) {
-    //            half::bits = _mm_cvtss_sh(value, _MM_FROUND_CUR_DIRECTION);
-    //            return;
-    //        }
-    //    #endif
-
-    //    constexpr
-    //    auto width = sizeof(T) * CHAR_BIT;
-
-    //    using uint_fast_t = uint_fastN_t<width>;
-    //    using uint_t = uintN_t<width>;
-
-    //    constexpr
-    //    auto frac_width = std::numeric_limits<T>::digits - 1;
-
-    //    constexpr
-    //    uint_fast_t tiny_exp_frac = uint_fast_t{1} << (frac_width - half::frac_width);
-
-    //    constexpr
-    //    uint_fast_t huge_exp_frac = uint_fast_t(half::max_exp - 1) << frac_width
-    //                              | half::max_frac << (frac_width - half::frac_width);
-
-    //    constexpr
-    //    uint_fast_t exp_frac_mask = (uint_fast_t{1} << (width - 1)) - 1;
-
-    //    unit_fast_t bits = std::bit_cast<uint_t>(x);
-    //    uint_fast_t exp_frac = bits & exp_frac_mask;
-
-    //    uint_fast_t underflow = -uint_fast_t(exp_frac < tiny_exp_frac);
-    //    uint_fast_t overflow = -uint_fast_t(exp_frac > huge_exp_frac);
-    //}
-
-    //half_to_single() {
-    //    #ifdef __F16C__
-    //        if (!std::is_constant_evaluated()) {
-    //            return _mm_cvtsh_ss(bits);
-    //        }
-    //    #endif
-    //}
 }
 
 struct half {
     template <typename T, std::enable_if_t<
-        std::is_floating_point_v<T>
+        std::numeric_limits<T>::is_iec559 && sizeof(T) * CHAR_BIT == 32
     >*...>
-    half(T value)
-    : bits(half_detail::bits_from_floating_point(value))
-    {}
+    half(T value) {
+        int ex;
 
-    template <typename T, std::enable_if_t<
-        std::is_integral_v<T> && !std::is_unsigned_v<T>
-    >*...>
-    half(T value)
-    : bits(half_detail::bits_from_integral(value))
-    {}
+        std::uint_fast32_t f = detail::bit_cast<std::uint32_t>(value);
 
-    template <typename T, std::enable_if_t<
-        std::is_integral_v<T> && std::is_unsigned_v<T>
-    >*...>
-    half(T value)
-    : bits(half_detail::bits_from_unsigned_integral(value))
-    {}
+        auto f_s = f & 0x80000000u;
+        auto f_e = f & 0x7F800000u;
+        auto f_m = f & 0x007FFFFFu;
+
+        std::uint_fast16_t h_s = f_s >> 16u;
+        std::uint_fast16_t h_e;
+        std::uint_fast16_t h_m;
+
+        auto e = detail::as_signed(f_e >> 23u) - 127;
+        if (e < -15 - 10) {
+            // Zero or a subnormal that will underflow to zero regardless of
+            // mantissa.
+
+            h_e = 0;
+            h_m = 0;
+
+            ex = FP_UNDERFLOW;
+        } else if (e <= -15) {
+            // Subnormal that might or might not underflow depending on
+            // mantissa.
+
+            h_e = 0;
+
+            auto wide_m = 0x800000u | f_m;
+
+            if (wide_m & ((1 << (-1 - e)) - 1)) {
+                ex = FP_UNDERFLOW;
+            }
+
+            auto m = wide_m >> detail::as_unsigned(-14 - e);
+
+            // Round to even.
+            if (((m & 0x3FFFu) ^ 0x1000u) | (wide_m & 0x7FFu)) {
+                m += 0x1000u;
+            }
+
+            h_m = m >> 13u;
+        }
+
+        half::bits = h_s | (h_e + h_m);
+
+        std::feraiseexcept(ex);
+    }
 
     half()
     = default;
 
-    template <typename T, std::enable_if_t<
-        std::is_floating_point_v<T>
-    >*...>
-    operator T()
-    const
-    { return half_detail::bits_to_floating_point<T>(bits); }
-
-    template <typename T, std::enable_if_t<
-        std::is_integral_v<T> && !std::is_unsigned_v<T>
-    >*...>
-    operator T()
-    const
-    { return half_detail::bits_to_integral<T>(bits); }
-
-    template <typename T, std::enable_if_t<
-        std::is_integral_v<T> && std::is_unsigned_v<T>
-    >*...>
-    operator T()
-    const
-    { return half_detail::bits_to_unsigned_integral<T>(bits); }
-
 private:
-    std::uint16_t bits;
+    std::int16_t bits;
 };
 
 static_assert(sizeof(half) * CHAR_BIT == 16);
 static_assert(std::is_trivial_v<half>);
-
-namespace std {
-    template <>
-    class numeric_limits<::half> {
-    public:
-
-
-    };
-}
 
 inline
 auto operator+(half x)
@@ -203,9 +98,9 @@ inline
 auto operator-(half x)
 -> half
 {
-    return half_detail::bit_cast<half>(
+    return detail::bit_cast<half>(
         static_cast<std::uint16_t>(
-            half_detail::bit_cast<std::uint16_t>(x) ^ 0b10000000u
+            detail::bit_cast<std::uint16_t>(x) ^ 0b10000000'00000000u
         )
     );
 }
@@ -214,9 +109,9 @@ inline
 auto fabs(half x)
 -> half
 {
-    return half_detail::bit_cast<half>(
+    return detail::bit_cast<half>(
         static_cast<std::uint16_t>(
-            half_detail::bit_cast<std::uint16_t>(x) & 0b01111111u
+            detail::bit_cast<std::uint16_t>(x) & 0b01111111'11111111u
         )
     );
 }
@@ -229,18 +124,21 @@ auto abs(half x)
 inline
 auto signbit(half x)
 -> bool
-{ return half_detail::bit_cast<std::uint16_t>(x) & 0b10000000u; }
+{ return detail::bit_cast<std::uint16_t>(x) & 0b10000000'00000000u; }
 
 inline
 auto copysign(half x, half y)
 -> half
 {
-    return half_detail::bit_cast<half>(
+    return detail::bit_cast<half>(
         static_cast<std::uint16_t>(
-            half_detail::bit_cast<std::uint16_t>(y) & 0b10000000u |
-            half_detail::bit_cast<std::uint16_t>(x) & 0b01111111u
+            detail::bit_cast<std::uint16_t>(y) & 0b10000000'00000000u |
+            detail::bit_cast<std::uint16_t>(x) & 0b01111111'11111111u
         )
     );
+}
+
+///////////////////////////////////////////////////////////////////////////////
 }
 
 #endif
